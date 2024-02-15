624de87374c43e50f5a2cd504cede3d3238bcd30
premanand98123 <premkr98123@gmail.com>
2024-02-15 15:49:10 +0530

    day 1

---
+++ intro_to_python.py
@@ -0,0 +1,64 @@
+# print("hello world")
+
+# a = 5
+# print(type(a))
+
+# a="oreo"
+# print(type(a))
+
+# age = int(input("Enter the age"))
+
+# if(age>=18):
+#     print("adult")
+    
+# elif(age>13):
+#     print("teenager")
+# else:
+#     print("child")
+
+'''
+def number_to_string(argument):
+    match argument:
+        case 0:
+            return "zero"
+        case 1:
+            return "one"
+        case 2:
+            return "two"
+        case default:
+            return "something"
+  
+ '''
+
+# s=45
+# a = [1,2,3,3,4,s,False]
+
+# a.append(67)
+
+
+
+
+
+# a=5
+# while(a<=10):
+#     print(a)
+#     a = a+1
+
+   
+List = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
+
+# a = len(List)
+# # a = 7
+
+# lidx = -a
+# # lidx = -7
+
+# uidx = -1
+# while(uidx>=lidx):
+#     print(List[uidx])
+#     uidx = uidx -1
+
+print(List)
+# print(List[::-1])
+
+print(List[2::2])
